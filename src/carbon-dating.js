e00a5875de260994d07497559771cc600ce30fd4
Artsem Hluzd <sv.temane@gmail.com>
2020-03-15 23:09:00 +0300

    do 3 tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,19 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  
+  // if (typeof sampleActivity != "string" || isNaN(sampleActivity) == true) return false;
+
+  if (typeof sampleActivity != "string") return false;
+  let sampleActivityNumber = parseFloat(sampleActivity);
+  if (isNaN(sampleActivityNumber) == true || sampleActivityNumber <= 0 || sampleActivityNumber > 15) return false;
+
+  let result = (Math.log(MODERN_ACTIVITY/sampleActivityNumber)/0.00012094);
+  return Math.ceil(result);
 };
+
+// 22392
+
+//3.838
+//2.1528404/HALF_LIFE_PERIOD
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,19 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  
+ 
+  let teamName = "";
+  let name = [];
+  
+  if (typeof members != 'object' || members == '') return false;
+  
+  for (let i = 0; i < members.length; i++) {
+    name[i] = members[i].replace(/\s+/g, '');
+    if (typeof name[i] == 'string' && name[i] != "")
+      teamName+=name[i][0].toUpperCase();
+  }
+  
+  let result = teamName.split('').sort().join('');
+  
+  
+  return result;
 };
